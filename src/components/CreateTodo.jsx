907c485cd1847e087c8b2ce4b80be192f80bf92a
rajareddykodakandla <rajareddykodakandla@gmail.com>
2025-01-04 18:50:34 -0500

    added a create todo component

---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,12 @@
+function CreateTodo() {
+  return (
+    <div>
+      <input type="text" placeholder="Title" />
+      <br />
+      <input type="text" placeholder="description" />
+      <br />
+
+      <button>Add TODO</button>
+    </div>
+  );
+}
